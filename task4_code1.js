570166dff6d04200e7c44f85314bf9bba2ec0e87
Ansochacko <ansochacko@gmail.com>
2025-05-01 02:54:08 +0530

    Delete task4_code1.js

--- task4_code1.js
+++
@@ -1,5 +0,0 @@
-let books = [
-  { "title": "Clean Code", "author": "Robert C. Martin", "year": 2008, "genre": "Software Engineering" },
-  { "title": "Eloquent JavaScript", "author": "Marijn Haverbeke", "year": 2018, "genre": "Programming" },
-  { "title": "HTML & CSS", "author": "Jon Duckett", "year": 2011, "genre": "Web Design" }
-];
